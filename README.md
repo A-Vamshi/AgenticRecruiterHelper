de976ec87fdc02e9f0e4a3642aade3dc32e3ccb2
Vamshi Adimalla <vamshiadimalla@Vamshis-MacBook-Air.local>
2025-06-02 18:49:11 +0530

    Do-over

--- README.md
+++
@@ -1,1 +0,0 @@
-Agentic recruitement screening scheduler
--- src/utils/job_description_extractor.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-from dotenv import load_dotenv
-load_dotenv()
-
-job_desc_path = os.getenv("JOB_DESCRIPTION_PATH")
-job_desc_df = pd.read_csv(job_desc_path, encoding="cp1252")
-job_desc_df = job_desc_df.drop("Unnamed: 2", axis=1)

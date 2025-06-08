9acabc9eb06f9876643337a24ca6af576acef485
Vamshi Adimalla <vamshiadimalla@Vamshis-MacBook-Air.local>
2025-06-08 15:36:32 +0530

    Added utils method to get resume info

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st 
 from utils.getJds import getJobDescriptions
-# jd_file = st.file_uploader("Upload JD CSV", type=["csv"])
-# resume_files = st.file_uploader("Upload PDF Resumes", type=["pdf"], accept_multiple_files=True)
+from utils.getResumeInfo import getResumeInfo
+jd_file = st.file_uploader("Upload JD CSV", type=["csv"])
+resume_files = st.file_uploader("Upload PDF Resumes", type=["pdf"], accept_multiple_files=True)
+
+if st.button("Get resume details"):
+    info = getResumeInfo(resume_files)
+    st.write(info)
+
+if st.button("Get jd details"):
+    info = getJobDescriptions(jd_file)
+    st.write(info)
